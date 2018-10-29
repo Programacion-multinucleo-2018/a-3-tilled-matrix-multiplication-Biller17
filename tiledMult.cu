56c6ebc39193c5075628c8d25f948d969e057cd2
Biller17 <abiller95@gmail.com>
2018-10-29 17:43:44 -0600

    final commit

--- tiledMult.cu
+++ tiledMult.cu
@@ -10,20 +10,17 @@ using namespace std;
 
 //Code used from examples and modified for activity
 //Adrian Biller A01018940
-//matrix multiplication with 2D 2D using tiled method
-
 
 
 
 
 //inicialization of matrices
-void initialData(int *ip, const int size)
+void initialData(float *ip, const int size)
 {
     int i;
-
     for(i = 0; i < size; i++)
     {
-        ip[i] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10));
+        ip[i] = 2;
     }
 
     return;
@@ -45,8 +42,7 @@ void printArray(int * arr, int size)
 
 
 //multiplication of matrices using cpu
-void multiplyMatrixOnHost(int *A, int *B, int *C, const int nx,
-                     const int ny)
+void multiplyMatrixOnHost(float *A, float *B, float *C, int nx, int ny)
 {
       for(int i = 0; i < nx; i++){
         for(int j = 0; j < nx ; j++){
@@ -60,7 +56,7 @@ void multiplyMatrixOnHost(int *A, int *B, int *C, const int nx,
 }
 
 //checking result of gpu and comparing them with cpu matrix
-void checkResult(int *hostRef, int *gpuRef, const int N)
+void checkResult(float *hostRef, float *gpuRef, const int N)
 {
     double epsilon = 1.0E-8;
     bool match = 1;
@@ -84,54 +80,66 @@ void checkResult(int *hostRef, int *gpuRef, const int N)
 
 
 
-//matrix calculation using cpu
-__global__ void multMatrixOnGPU2D(int *MatA, int *MatB, int *MatC, int nx, int ny)
-{
-    unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
-    unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
-    unsigned int idx = iy * nx + ix;
-
-
-    if (ix < nx && iy < ny){
-        for(int k = 0; k < nx; k++){
-          MatC[ix * nx + iy] += MatA[ix * nx + k] * MatB[k * nx + iy];
-        }
-    }
-}
+// //matrix calculation using cpu
+// __global__ void multMatrixOnGPU2D(float *MatA, float *MatB, float *MatC, int nx, int ny)
+// {
+//     unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
+//     unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
+//     unsigned int idx = iy * nx + ix;
+//
+//
+//     if (ix < nx && iy < ny){
+//         for(int k = 0; k < nx; k++){
+//           MatC[ix * nx + iy] += MatA[ix * nx + k] * MatB[k * nx + iy];
+//         }
+//     }
+// }
 
 //matrix calculation using tile method
-__global__ void tiledMult(int *MatA, int *MatB, int *MatC, int nx, int ny)
+__global__ void tiledMult(float *MatA, float *MatB, float *MatC, int nx, int ny)
 {
-    unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
-    unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
+    unsigned int ix = threadIdx.x + blockIdx.x * TILEDIM;
+    unsigned int iy = threadIdx.y + blockIdx.y * TILEDIM;
     // unsigned int idx = iy * nx + ix;
 
     __shared__ float sharedMatA[TILEDIM][TILEDIM];
     __shared__ float sharedMatB[TILEDIM][TILEDIM];
 
-  
-    int ty = threadIdx.y
-    int tx = threadIdx.x
 
-    //vamos a traves de todos los tiles
-    for(int i = (TILEDIM + nx - 1)/DIM; i >= 0; i--) {
-      if((i * TILEDIM + threadIdx.x) < nx && (iy < ny)) {
-        sharedMatA[ty][tx] = A[(iy*ny) + (i*TILEDIM+tx)];
+    int ty = threadIdx.y;
+    int tx = threadIdx.x;
+    float sum = 0;
+
+    //initializing tile in 0s
+    for(int i = 0; i <= TILEDIM; i ++) {
+      for(int j = 0; j <= TILEDIM; j++) {
+        sharedMatA[i][j] = 0;
+        sharedMatA[i][j] = 0;
+      }
+    }
+
+    for(int i = (TILEDIM + nx - 1)/TILEDIM; i >= 0; i--) {
+
+      if((i * TILEDIM + tx) < nx && (iy < ny)) {
+        sharedMatA[ty][tx] = MatA[(iy*ny) + (i*TILEDIM+tx)];
       }
 
-      if((i * DIM + threadIdx.y) < ny && (ix < nx)) {
-        sharedMatB[ty][tx] = B[(i*TILEDIM+ty) * nx + ix];
+      if((i * TILEDIM + ty) < ny && (ix < nx)) {
+        sharedMatB[ty][tx] = MatB[(i*TILEDIM+ty) * nx + ix];
       }
 
+
+      //syncing threads and getting final value for result matrix
       __syncthreads();
       for(int j = 0; j < TILEDIM; j++) {
-        sum += matTempA[ty][j] * matTempB[j][tx];
+        sum += sharedMatA[ty][j] * sharedMatB[j][tx];
       }
       __syncthreads();
     }
 
+    //writing value in result matrix
     if(ix < nx && iy < ny) {
-      C[iy*ny+ix] = sum;
+      MatC[iy*ny+ix] = sum;
     }
 }
 
@@ -151,19 +159,19 @@ int main(int argc, char **argv)
     // set up data size of matrix
     // int nx = 1 << 12;
     // int ny = 1 << 12;
-    int nx = 2000;
-    int ny = 2000;
+    int nx = 500;
+    int ny = 500;
 
     int nxy = nx * ny;
-    int nBytes = nxy * sizeof(int);
+    int nBytes = nxy * sizeof(float);
     printf("Matrix size: nx %d ny %d\n", nx, ny);
 
     // malloc host memory
-    int *h_A, *h_B, *hostRef, *gpuRef;
-    h_A = (int *)malloc(nBytes);
-    h_B = (int *)malloc(nBytes);
-    hostRef = (int *)malloc(nBytes);
-    gpuRef = (int *)malloc(nBytes);
+    float *h_A, *h_B, *hostRef, *gpuRef;
+    h_A = (float *)malloc(nBytes);
+    h_B = (float *)malloc(nBytes);
+    hostRef = (float *)malloc(nBytes);
+    gpuRef = (float *)malloc(nBytes);
 
     // initialize data at host side
 
@@ -177,16 +185,14 @@ int main(int argc, char **argv)
     memset(hostRef, 0, nBytes);
     memset(gpuRef, 0, nBytes);
 
-    // add matrix at host side for result SAFE_CALLs
+    //add matrix at host side for result SAFE_CALLs
     auto start_cpu =  chrono::high_resolution_clock::now();
     multiplyMatrixOnHost(h_A, h_B, hostRef, nx, ny);
     auto end_cpu =  chrono::high_resolution_clock::now();
     chrono::duration<float, std::milli> duration_ms = end_cpu - start_cpu;
 
-    printf("multiplyMatrixOnHost elapsed %f ms\n", duration_ms.count());
-
     // malloc device global memory
-    int *d_MatA, *d_MatB, *d_MatC;
+    float *d_MatA, *d_MatB, *d_MatC;
     SAFE_CALL(cudaMalloc((void **)&d_MatA, nBytes), "Error allocating d_MatA");
     SAFE_CALL(cudaMalloc((void **)&d_MatB, nBytes), "Error allocating d_MatB");
     SAFE_CALL(cudaMalloc((void **)&d_MatC, nBytes), "Error allocating d_MatC");
@@ -203,21 +209,20 @@ int main(int argc, char **argv)
     dim3 grid((nx + block.x - 1) / block.x, (ny + block.y - 1) / block.y);
 
     start_cpu =  chrono::high_resolution_clock::now();
-    multMatrixOnGPU2D<<<grid, block>>>(d_MatA, d_MatB, d_MatC, nx, ny);
+    tiledMult<<<grid, block>>>(d_MatA, d_MatB, d_MatC, nx, ny);
     SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel");
     end_cpu =  chrono::high_resolution_clock::now();
-
     duration_ms = end_cpu - start_cpu;
-
-    printf("sumMatrixOnGPU1D <<<(%d,%d), (%d,%d)>>> elapsed %f ms\n", grid.x,
-           grid.y,
-           block.x, block.y, duration_ms.count());
-
-    // SAFE_CALL kernel error
-    SAFE_CALL(cudaGetLastError(), "Error with last error");
-
-    // copy kernel result back to host side
-    SAFE_CALL(cudaMemcpy(gpuRef, d_MatC, nBytes, cudaMemcpyDeviceToHost), "Error copying d_MatC");
+    //
+    // printf("sumMatrixOnGPU1D <<<(%d,%d), (%d,%d)>>> elapsed %f ms\n", grid.x,
+    //        grid.y,
+    //        block.x, block.y, duration_ms.count());
+    //
+    // // SAFE_CALL kernel error
+    // SAFE_CALL(cudaGetLastError(), "Error with last error");
+    //
+    // // copy kernel result back to host side
+    // SAFE_CALL(cudaMemcpy(gpuRef, d_MatC, nBytes, cudaMemcpyDeviceToHost), "Error copying d_MatC");
 
     // printArray(hostRef, nx);
     // printf("Host\n");
@@ -227,13 +232,13 @@ int main(int argc, char **argv)
     // checkResult(hostRef, gpuRef, nxy);
 
 
-    //calculating matrix multiplication using Tiling
-    start_cpu =  chrono::high_resolution_clock::now();
-    tiledMult<<<grid, block>>>(d_MatA, d_MatB, d_MatC, nx, ny);
-    SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel");
-    end_cpu =  chrono::high_resolution_clock::now();
-
-    duration_ms = end_cpu - start_cpu;
+    // //calculating matrix multiplication using Tiling
+    // start_cpu =  chrono::high_resolution_clock::now();
+    // tiledMult<<<grid, block>>>(d_MatA, d_MatB, d_MatC, nx, ny);
+    // SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel");
+    // end_cpu =  chrono::high_resolution_clock::now();
+    //
+    // duration_ms = end_cpu - start_cpu;
 
     printf("Matrix multiplication with tiling <<<(%d,%d), (%d,%d)>>> elapsed %f ms\n", grid.x, grid.y, block.x, block.y, duration_ms.count());
 
